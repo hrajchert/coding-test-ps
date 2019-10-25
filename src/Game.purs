54d340bf36716679b7332d3c36ca0e0862611b86
Hernan Rajchert <hrajchert@gmail.com>
2019-10-25 12:38:44 -0300

    Refactor game to sort the code by dependence

--- src/Game.purs
+++ src/Game.purs
@@ -7,41 +7,17 @@ import Data.Either (Either(..))
 import Effect.Exception (error)
 import Foreign (Foreign)
 import Enquirer (PromptOptions(..), promptMultiple)
-import Simple.JSON (read, class ReadForeign)
 
 import Capability.PrintOutput (class PrintOutput, print)
 import Capability.PromptInput (class PromptInput, BoatInfo, promptBoatInfo)
 
 
-runGame :: forall m
-     . PrintOutput m
-    => PromptInput m
-    => State
-    -> m State
-runGame state = do
-    -- In each step we start by showing the current state
-    print $ show state
-    -- We ask the user input and calculate the newState
-    boatInfo <- promptBoatInfo $ boatDirection state
-    case (step state boatInfo) of
-      -- If there are some errors we print them and rerun in the same state
-      Left err -> do
-        print $ "We cant do that because: " <> show err
-        runGame state
-      -- If we finish we return the final step, if not we recurse
-      Right s  -> if (isFinalState s)
-        then do
-          print "Congratulations! You have solved the puzzle"
-          pure s
-        else runGame s
-
 type Monkeys = Int
-
 type Wolfs = Int
-
 
 type BoatPassengerCount = { monkeys :: Monkeys, wolfs :: Wolfs }
 
+-- Convert the user input into something we can operate with (integers)
 countPassengers :: BoatInfo -> BoatPassengerCount
 countPassengers {first, second} =
   let
@@ -54,13 +30,13 @@ countPassengers {first, second} =
     , wolfs:  (countWolf first + countWolf second)
     }
 
+
 -- TODO: maybe make name more strict (initial | final)
 newtype Shore = Shore {name :: String, monkeys :: Monkeys, wolfs :: Wolfs}
 derive instance eqShore :: Eq Shore
 
 instance shoreShow :: Show Shore where
   show (Shore {name, monkeys, wolfs}) = "the " <> name <> " shore has " <> show monkeys <> " monkeys and " <> show wolfs <> " wolfs"
-
 
 data State
   = BoatForward Shore Shore
@@ -76,9 +52,10 @@ boatDirection :: State -> String
 boatDirection (BoatForward _ _)  = "forward"
 boatDirection (BoatBackward _ _) = "backward"
 
+-- If the boat is moving backwards and there are no animals in the initial shore,
+-- then we reached a final state
 isFinalState :: State -> Boolean
 isFinalState (BoatForward _ _) = false
--- If the boat is moving backwards and there are no animals in the initial shore, then we reached a final state
 isFinalState (BoatBackward (Shore {monkeys, wolfs}) _) = monkeys == 0 && wolfs == 0
 
 data InvalidMove
@@ -93,7 +70,29 @@ instance showInvalidMove :: Show InvalidMove where
   show NotEnoughWolfs = "There aren't that many wolfs to move to the other shore"
   show (MoreWolfsThanMonkeys (Shore shore)) = "There are " <> show shore.wolfs <> " wolfs and " <> show shore.monkeys <> " monkeys in the " <> shore.name <> " shore, tragedies may happen"
 
+-------------------------------------------------------------------------------
+-- Game logic
 
+-- Validate that in a shore there can't be more wolfs than monkeys
+validateShore :: Shore -> Either InvalidMove Shore
+validateShore (Shore s) =
+  if s.monkeys > 0 && s.wolfs > s.monkeys
+    then Left $ MoreWolfsThanMonkeys $ Shore s
+    else Right $ Shore s
+
+
+-- Remove the animals from the shore, validating that there are enough animals and that the shore is still valid after
+moveAnimalsToBoat :: Shore -> BoatPassengerCount -> Either InvalidMove Shore
+moveAnimalsToBoat (Shore from) count =
+  if count.monkeys > from.monkeys
+      then Left NotEnoughMonkeys
+      else if count.wolfs > from.wolfs
+        then Left NotEnoughWolfs
+        else validateShore (Shore {name: from.name, monkeys: from.monkeys - count.monkeys, wolfs: from.wolfs - count.wolfs})
+
+-- Insert animals into the shore, validating that the result is still valid
+moveAnimalsToShore :: Shore -> BoatPassengerCount -> Either InvalidMove Shore
+moveAnimalsToShore (Shore to) count = validateShore (Shore {name: to.name, monkeys: to.monkeys + count.monkeys, wolfs: to.wolfs + count.wolfs})
 
 step :: State -> BoatInfo -> Either InvalidMove State
 step (BoatForward initial final) boat = do
@@ -109,24 +108,26 @@ step (BoatBackward initial final) boat = do
   newInitialShore <- moveAnimalsToShore initial count
   Right $ (BoatForward  newInitialShore newFinalShore)
 
--- Remove the animals from the shore, validating that there are enough animals and that the shore is still valid after
-moveAnimalsToBoat :: Shore -> BoatPassengerCount -> Either InvalidMove Shore
-moveAnimalsToBoat (Shore from) count =
-  if count.monkeys > from.monkeys
-      then Left NotEnoughMonkeys
-      else if count.wolfs > from.wolfs
-        then Left NotEnoughWolfs
-        else validateShore (Shore {name: from.name, monkeys: from.monkeys - count.monkeys, wolfs: from.wolfs - count.wolfs})
-
--- Insert animals into the shore, validating that the result is still valid
-moveAnimalsToShore :: Shore -> BoatPassengerCount -> Either InvalidMove Shore
-moveAnimalsToShore (Shore to) count = validateShore (Shore {name: to.name, monkeys: to.monkeys + count.monkeys, wolfs: to.wolfs + count.wolfs})
-
--- Validate that in a shore there can't be more wolfs than monkeys
-validateShore :: Shore -> Either InvalidMove Shore
-validateShore (Shore s) =
-  if s.monkeys > 0 && s.wolfs > s.monkeys
-    then Left $ MoreWolfsThanMonkeys $ Shore s
-    else Right $ Shore s
-
-
+runGame :: forall m
+   . PrintOutput m
+  => PromptInput m
+  => State
+  -> m State
+runGame state = do
+   -- In each step we start by showing the current state
+   print $ show state
+   -- We ask the user input and calculate the newState
+   boatInfo <- promptBoatInfo $ boatDirection state
+   case (step state boatInfo) of
+     -- If there are some errors we print them and rerun in the same state
+     Left err ->
+       do
+         print $ "We cant do that because: " <> show err
+         runGame state
+     -- If we finish we return the final step, if not we recurse
+     Right s  ->
+       if (isFinalState s)
+         then do
+           print "Congratulations! You have solved the puzzle"
+           pure s
+         else runGame s
